c9e0bc694e504a7e03827485f122b9bffb600ba5
Coleiosis <Coleiosis6@gmail.com>
2018-10-03 08:21:42 -0600

    SOlved to equations using cramer's rule

---
+++ Chapter 1/Ex_1_13.java
@@ -0,0 +1,12 @@
+/*
+Athor: Cole Buehler
+Date 10/3/2018
+
+Output result of a equation with variables
+*/
+class Ex_1_13 {
+	public static void main(String[] args) {
+		System.out.println(((44.5 * 0.55) - (50.2 * 5.9)) / ((3.4 * 0.55) - (50.2 * 2.1)));
+		System.out.println(((3.4 * 5.9) - (44.5 * 2.1)) / ((3.4 * 0.55) - (50.2 * 2.1)));
+	}
+}
