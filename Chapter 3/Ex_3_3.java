05ca895589241157b95db365a575380fab8a96e1
Coleiosis <60004488@ironschools.org>
2018-10-15 08:56:56 -0600

    Used cramer's rule to sp;ve linear expression

---
+++ Chapter 3/Ex_3_3.java
@@ -0,0 +1,28 @@
+/*
+Author: Cole Buehler
+Date: 10/15/2018
+
+Uses Cramer's Rule to solve linear expressions
+*/
+import java.util.Scanner;
+
+	public static void main(String[] args) {
+		// Declare variables
+		Scanner input = new Scanner(System.in);
+		double a, b, c, d, e, f, x, y = 0.0;
+		
+		// Prompt the user
+		System.out.println("We are going to solve a linear expression using cramer's rule")
+		System.out.print ("Enter the folowing values")
+		
+		// Check that denominator is not zero
+		if (ad - bc !=0) {
+			
+			// Solve for x and y
+			
+		}
+		else {
+			System.out.print("Cannot divide by zero");
+		}
+	}
+}
