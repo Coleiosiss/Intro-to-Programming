b81451077aa716d9d156bca6ad45bd3899c618a6
404kevnotfound <student@HSiMac4-16.local>
2018-10-26 08:59:55 -0600

    Created Ex_4_17

---
+++ Chapter 4/Ex_4_17.java
@@ -0,0 +1,37 @@
+/*
+Author: Cole Buehler
+10/26/2018
+
+Create a program to determine how many days a month has in a given year
+*/
+import java.util.Scanner;
+
+class Ex_4_17 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a year: ");
+		Double year = input.nextDouble();
+		Double jan;
+		Double feb;
+		Double mar;
+		Double apr; 
+		Double may;
+		Double jun;
+		Double jul; 
+		Double aug;
+		Double sep;
+		Double oct;
+		Double nov;
+		Double dec;
+		System.out.print("Enter a month: ");
+		Double month = input.nextDouble();
+		if(jan || mar || may || jul || aug || oct || dec);
+		else if(apr || jun || sep || nov);
+		{
+		System.out.print("31 days");
+		System.out.print("30 days");
+		}
+		
+
+	}
+}
