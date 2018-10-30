ac211c0aa31faabc53b142eaa1b75b4fc5825303
Nunya Buisness <student@HSiMac4-16.local>
2018-10-30 08:59:26 -0600

    Created Ex_2_23

--- Chapter 3/Ex_3_23.java
+++ Chapter 3/Ex_3_23.java
@@ -1,23 +1,25 @@
 /*
+Author: Cole Buehler
+Date: 10/30/2018
+
+Write a program to determine if a point is in a rectangle
 */
+
 import java.util.Scanner;
 
-public class Exercise_03_23 {
+class Ex_3_23 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-
-		// Prompt the user to enter a point (x, y)
-		System.out.print("Enter a point with two coordinates: ");
-		double x = input.nextDouble();
-		double y = input.nextDouble();
-
-		// Check whether the point is within the rectangle
-		// centered at (0, 0) with width 10 and height 5
-		boolean withinRectangle = (Math.pow(Math.pow(x, 2), 0.5) <= 10 / 2 ) ||
-										  (Math.pow(Math.pow(y, 2), 0.5) <= 5.0 / 2);
-
-		// Display results
-		System.out.println("Point (" + x + ", " + y + ") is " +
-			((withinRectangle) ? "in " : "not in ") + "the rectangle");
+		
+		//Prompt user to enter Point x and Point y
+		System.out.println("Enter 2 points to see if its in a given rectangle: ");
+		System.out.print("Point x: ");
+		double pointx = input.nextDouble();
+		System.out.print("Point y: ");
+		double pointy = input.nextDouble();
+		
+		//Test if points are in the rectangle
+	
+		
 	}
 }
